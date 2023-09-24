073032785ee4f54ca4a73523fdea3291548a42f1
zurichscud <zurichscud@outlook.com>
2023-09-24 16:04:15 +0800

    set SpringMVC env

---
+++ src/main/java/com/example/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: zurichscud
+ * @Date: 2023/9/24 15:43
+ * @Description: TODO
+ */
+@Controller
+public class MainController {
+    @ResponseBody
+    @RequestMapping("/")
+    public String hello(){
+        return "HelloWorld!!!!!!!!!!!!!!!!!!!!";
+    }
+}
